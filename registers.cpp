d96b496b2542aef1f4b12ab2dd3d7f1d50710964
Rafzy <rafze04@gmail.com>
2025-05-26 16:56:22 +0900

    Update rm table

--- registers.cpp
+++ registers.cpp
@@ -5,5 +5,5 @@ using namespace std;
 string reg_table[][8] = {{"AL", "CL", "DL", "BL", "AH", "CH", "DH", "BH"},
                          {"AX", "CX", "DX", "BX", "SP", "BP", "SI", "DI"}};
 
-string rm_table[] = {"(BX) + (SI)", "(BX) + (DI)", "(BP) + (SI)", "(BP) + (DI)",
-                     "(SI)",        "(DI)",        "(BP)",        "(BX)"};
+string rm_table[] = {"[BX + SI", "[BX + DI", "[BP + SI", "[BP + DI",
+                     "[SI",      "[DI",      "[BP",      "[BX"};
